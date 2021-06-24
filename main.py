e49d5af3143c3c9e5f842cfddf0eddb0c5ec8a5f
lmd578 <73003097+lmd578@users.noreply.github.com>
2021-06-24 19:37:04 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.c = 'a + b * i'
+
+    def __add__(self, other):
+        print(f'Сумма z1 и z2 равна')
+        return f'z = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение z1 и z2 равно')
+        return f'c = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'c = {self.a} + {self.b} * i'
+
+
+z_1 = ComplexNumber(3, 1)
+z_2 = ComplexNumber(9, -4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
